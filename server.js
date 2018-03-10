0f946a2f39dc5effd804c303fc2d93c19b0f8ab6
Alliance <574014131@qq.com>
2018-03-10 14:24:44 +0800

    add user name

--- server.js
+++ server.js
@@ -1,29 +1,42 @@
-var express=require('express');
-var fs=require('fs');
-var app=express();
-var userNames=[];
+var express=require('express')
+var fs=require('fs')
+var app=express()
+var userNames=[]
 app.get('/',function (req, res) {
     fs.readFile('./index.html',function (error,data) {
-        res.writeHead(200,{'Content-Type':'text/html'});
+        res.writeHead(200,{'Content-Type':'text/html'})
         res.end(data,'utf-8')
     })
-});
+})
 var server=app.listen(3000,function () {
     console.log('Server is listening at http://127.0.0.1')
-});
-var io=require('socket.io').listen(server);
+})
+var io=require('socket.io').listen(server)
 io.sockets.on('connection',function (socket) {
-    socket.emit('welcome',{text:'Welcome'});
-    socket.on('name',function (data) {
-        console.log(`Server received the following name : ${data.userName}`);
-        userNames.push(data.userName);
-        socket.userName=data.userName;
+    socket.emit('welcome',userNames)
+    socket.on('name',function (data,callback) {
+        console.log(`Server received the following name : ${data.userName}`)
+        if(userNames.indexOf(data.userName)!== -1){
+            callback(false)
+        }else{
+            callback(true)
+            userNames.push(data.userName)
+            socket.userName=data.userName
+            io.sockets.emit('userNames',userNames)
+        }
     });
+    socket.on('message',function (data) {
+        socket.emit('message',{
+            userName:socket.userName,
+            message:data
+        })
+    })
     socket.on('disconnect',function () {
         if(!socket.userName) return;
         if(userNames.indexOf(socket.userName)>-1){
-            userNames.splice(userNames.indexOf(socket.userName),1);
-            console.log(userNames);
+            userNames.splice(userNames.indexOf(socket.userName),1)
+            console.log(userNames)
         }
+        socket.broadcast.emit('userNames',userNames)
     })
 });
